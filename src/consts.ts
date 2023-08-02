1b66f33a824a1df5431105f0f3c74197488bcc93
Alex Tuppen <alex.tuppen@gmail.com>
2023-08-02 21:38:14 +0100

    Port global styles Begin building Header Remove RSS

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,15 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Alex Tuppen";
+export const SITE_DESCRIPTION =
+  "Personal website of Alex Tuppen, thoughts on software development and recipes I create and want to remember";
+
+export const URL_LINKEDIN = "https://www.linkedin.com/in/alextuppen/";
+export const URL_INSTAGRAM = "https://www.instagram.com/alextuppen/";
+export const URL_GITHUB = "https://github.com/alextuppen/";
+
+export const BP_QUERY_SMALL = "(min-width: 37.5rem)";
+export const BP_QUERY_MEDIUM = "(min-width: 56.25rem)";
+export const BP_QUERY_LARGE = "(min-width: 75rem)";
+export const BP_QUERY_XL = "(min-width: 112.5rem)";
