e4da9a35bd1c408454f433fc717f086bac6995c7
Alex Tuppen <alex.tuppen@gmail.com>
2024-01-08 22:26:36 +0000

    Implement recipe schema

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,7 @@
 import { defineCollection, z } from "astro:content";
 
 const blog = defineCollection({
+  type: "content",
   // Type-check frontmatter using a schema
   schema: z.object({
     title: z.string(),
@@ -19,4 +20,52 @@ const blog = defineCollection({
   }),
 });
 
-export const collections = { blog };
+const recipes = defineCollection({
+  type: "data",
+  schema: z.object({
+    "@context": z.literal("https://schema.org/"),
+    "@type": z.literal("Recipe"),
+    name: z.string(),
+    image: z.string().array().optional(),
+    author: z.object({
+      "@type": z.literal("Person"),
+      name: z.literal("Alex Tuppen"),
+    }),
+    datePublished: z
+      .string()
+      .or(z.date())
+      .transform((val) => new Date(val)),
+    description: z.string(),
+    creativeWorkStatus: z.union([z.literal("draft"), z.literal("published")]),
+    prepTime: z.string(),
+    cookTime: z.string(),
+    totalTime: z.string(),
+    keywords: z.string(),
+    recipeYield: z.number(),
+    recipeCategory: z.literal("Weeknight meal"),
+    recipeIngredient: z.string().array(),
+    tool: z.array(
+      z.object({ "@type": z.literal("HowToTool"), name: z.string() })
+    ),
+    recipeInstructions: z.array(
+      z.object({
+        "@type": z.literal("HowToStep"),
+        name: z.string(),
+        text: z.string().optional(),
+        itemListElement: z
+          .array(
+            z.object({
+              "@type": z.union([
+                z.literal("HowToDirection"),
+                z.literal("HowToTip"),
+              ]),
+              text: z.string(),
+            })
+          )
+          .optional(),
+      })
+    ),
+  }),
+});
+
+export const collections = { blog, recipes };
