96bcb72a90aba59814fc0501cf977091c9487e99
Alex Tuppen <alex.tuppen@gmail.com>
2024-02-26 23:15:30 +0000

    Break up recipe layout into smaller individual components

---
+++ src/components/Display/Recipe/index.ts
@@ -0,0 +1,6 @@
+import Draft from "./Draft.astro";
+import Method from "./Method.astro";
+import TimesServes from "./TimesServes.astro";
+import ToolsIngredients from "./ToolsIngredients.astro";
+
+export { Draft, Method, TimesServes, ToolsIngredients };
--- src/components/Display/index.ts
+++ src/components/Display/index.ts
@@ -1,5 +1,7 @@
 import BlogRecipeList from "./BlogRecipeList.astro";
 import FormattedDate from "./FormattedDate.astro";
+import HorizontalSeperator from "./HorizontalSeperator.astro";
 
 export { Education, ExpEduDetails, Experiences } from "./Experience";
-export { BlogRecipeList, FormattedDate };
+export { Draft, Method, TimesServes, ToolsIngredients } from "./Recipe";
+export { BlogRecipeList, FormattedDate, HorizontalSeperator };
