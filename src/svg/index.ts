276d8ad5b16d2ea75eed9fa25c7327095d9a9a2a
Alex Tuppen <alex.tuppen@gmail.com>
2024-03-01 15:00:44 +0000

    Add cards to recipe page Add icons to times serves

---
+++ src/svg/index.ts
@@ -0,0 +1,17 @@
+import Clock from "./Clock.astro";
+import CookingPot from "./CookingPot.astro";
+import ExclamationCircle from "./ExclamationCircle.astro";
+import Knife from "./Knife.astro";
+import KnifeFork from "./KnifeFork.astro";
+import LightBulb from "./LightBulb.astro";
+import MissingImage from "./MissingImage.astro";
+
+export {
+  Clock,
+  CookingPot,
+  ExclamationCircle,
+  Knife,
+  KnifeFork,
+  LightBulb,
+  MissingImage,
+};
