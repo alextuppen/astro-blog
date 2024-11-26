37db814b6f30e27f20e68f13a82a9443f6e6c50b
Alex Tuppen <alex.tuppen@gmail.com>
2024-11-26 21:19:33 +0000

    Create Hero component

--- src/components/Astro/index.ts
+++ src/components/Astro/index.ts
@@ -1,18 +1,20 @@
-import FormattedDate from "./FormattedDate.astro";
-import HorizontalSeperator from "./HorizontalSeperator.astro";
 import BaseHead from "./BaseHead.astro";
 import Button from "./Button.astro";
 import Footer from "./Footer.astro";
+import FormattedDate from "./FormattedDate.astro";
+import Hero from "./Hero.astro";
+import HorizontalSeperator from "./HorizontalSeperator.astro";
 import Section from "./Section.astro";
 
 export { Education, Job } from "./Experience";
 export { Draft, Method, TimesServes, ToolsIngredients } from "./Recipe";
 export { Header } from "./Header";
 export {
-  Button,
-  FormattedDate,
-  HorizontalSeperator,
   BaseHead,
+  Button,
   Footer,
+  FormattedDate,
+  Hero,
+  HorizontalSeperator,
   Section,
 };
--- src/components/index.ts
+++ src/components/index.ts
@@ -12,5 +12,6 @@ export {
   Footer,
   Header,
   Section,
+  Hero,
 } from "./Astro";
 export { BlogRecipeList, BlogRecipeItem } from "./Solid";
