49d110561a38d3019a2598d72cadfb34837e0e83
Alex Tuppen <alex.tuppen@gmail.com>
2024-12-03 17:38:36 +0000

    working digital rain!

--- src/components/Astro/Hero/HeroBackground.types.ts
+++ src/components/Astro/Hero/HeroBackground.types.ts
@@ -1,6 +1,9 @@
 export interface Column {
   chars: string[];
   currentChar: number;
+  charsRemaining: number;
+  gradient: number[];
+  fontSize: number;
   xCoord: number;
   yCoord: number;
   prevRender: number;
